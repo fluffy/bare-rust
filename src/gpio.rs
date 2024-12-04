deec7948909a5c2b7dab401466c6b67d63a2d35c
Cullen Jennings <fluffy@iii.ca>
2024-12-04 13:21:54 -0500

    add macro

--- src/gpio.rs
+++ src/gpio.rs
@@ -11,7 +11,7 @@ pub enum Bank {
 pub struct Pin(Bank, u8);
 
 #[repr(C)]
-struct GpioReg {
+pub struct GpioReg {
     moder: u32,
     otyper: u32,
     ospeedr: u32,
@@ -24,6 +24,10 @@ struct GpioReg {
     afrh: u32,
     brr: u32,
 }
+
+pub const GPIO_A: *mut GpioReg = (0x4800_0800 + 0x400 * 0) as *mut GpioReg;
+pub const GPIO_B: *mut GpioReg = (0x4800_0800 + 0x400 * 1) as *mut GpioReg;
+pub const GPIO_C: *mut GpioReg = (0x4800_0800 + 0x400 * 2) as *mut GpioReg;
 
 fn gpio(b: Bank) -> *mut GpioReg {
     let bank_num = b as u8;
@@ -44,6 +48,13 @@ fn write_2bits(addr: *mut u32, num: u8, data: u32) {
         ptr::write_volatile(addr, v);
     }
 }
+
+macro_rules! write_2bits {
+    ($x:ident.$y:ident, $bit_num:expr, $val:expr ) => {
+        unsafe { write_2bits(ptr::addr_of_mut!((*$x).$y), $bit_num, $val) }
+    };
+}
+
 fn init_output(p: Pin) {
     let gpio = gpio(p.0);
     let pin_num = p.1;
@@ -52,6 +63,8 @@ fn init_output(p: Pin) {
         // set mode to output
         write_2bits(ptr::addr_of_mut!((*gpio).moder), pin_num, 0x10);
     }
+
+    write_2bits!(GPIO_A.moder, pin_num, 0x10);
 }
 
 pub fn init() {
