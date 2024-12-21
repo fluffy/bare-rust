1277ae4e78d44328c7b964dbcc9ff6abf0167f80
Cullen Jennings <fluffy@iii.ca>
2024-12-21 14:18:00 -0700

    Get main to compile for bare and not bare arch

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-#![no_std]
-#![no_main]
-
-use core::panic::PanicInfo;
+#![cfg_attr(  target_arch = "arm", no_std )]
+#![cfg_attr(  target_arch = "arm", no_main )]
 
 use hal;
 use hal::debug;
@@ -11,15 +9,44 @@ use hal::led::Color;
 mod startup;
 //mod hal;
 
+#[cfg(target_arch = "arm")]
+use core::panic::PanicInfo;
+#[cfg(target_arch = "arm")]
 #[panic_handler]
 fn panic(_panic: &PanicInfo) -> ! {
     loop {}
 }
 
+//#[cfg(not(target_arch = "arm"))]
+//use core::panic::PanicInfo;
+//#[cfg(not(target_arch = "arm"))]
+//#[panic_handler]
+//fn panic(_panic: &PanicInfo) -> ! {
+//    loop {}
+//}
+
+
+#[cfg(target_arch = "arm")]
 #[no_mangle]
+//#[start]
 #[export_name = "main"]
 #[inline(never)]
 pub extern "C" fn main() -> ! {
+    my_main();
+}
+
+
+
+#[cfg(not(target_arch = "arm"))]
+//#[start]
+//#[export_name = "main"]
+fn main() -> () {
+    my_main();
+}
+
+
+
+fn my_main() -> ! {
     hal::init();
 
     loop {
