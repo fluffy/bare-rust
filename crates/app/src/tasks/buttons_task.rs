9d9cac65c2bad8263480c449adeef74e6425e271
Cullen Jennings <fluffy@iii.ca>
2024-12-31 11:52:18 -0700

    update budgets

--- crates/app/src/tasks/buttons_task.rs
+++ crates/app/src/tasks/buttons_task.rs
@@ -9,8 +9,8 @@ pub struct ButtonTask {
 const BUTTON_TASK_INFO: TaskInfo = TaskInfo {
     name: "Button",
     run_every_ms: 100,
-    time_budget_us: 100,
-    mem_budget_bytes: 50,
+    time_budget_us: 5000,
+    mem_budget_bytes: 500,
 };
 
 impl Task for ButtonTask {
