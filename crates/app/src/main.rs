7ab8065844a70651f936b4573696a2aee712791a
Cullen Jennings <fluffy@iii.ca>
2024-12-31 10:04:48 -0700

    refactor dispatch

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -17,6 +17,7 @@ use dev::led::Color;
 //use crate::msg::Msg;
 
 mod channel;
+mod dispatch;
 mod msg;
 mod stack;
 mod startup;
@@ -33,19 +34,6 @@ pub extern "C" fn main() -> ! {
 #[cfg(feature = "std")]
 fn main() -> () {
     my_main();
-}
-
-fn dispatch(msg: msg::Msg) {
-    match msg {
-        msg::Msg::PttButton(pressed) => {
-            if pressed {
-                b"  PTT button pressed\r\n".print_console();
-            } else {
-                b"  PTT button released\r\n".print_console();
-            }
-        }
-        _ => {}
-    }
 }
 
 #[inline(never)]
@@ -69,56 +57,21 @@ fn my_main() -> ! {
     let mut task_mgr = tasks::TaskMgr::new(&mut sender, &mut bsp);
     task_mgr.add_task(&button_task);
 
+    led::set(Color::Green);
+
+    let stack_usage = stack::usage() as u32;
+
+    if cfg!(not(feature = "std")) {
+        b"  Starting stack usage: ".print_console();
+        stack_usage.print_console();
+        b" bytes\r\n".print_console();
+    }
+
+    fib_test();
+
     loop {
-        led::set(Color::Green);
-
         task_mgr.run();
-
-        loop {
-            let msg = receiver.recv();
-            if msg == msg::Msg::None {
-                break;
-            }
-            dispatch(msg);
-        }
-
-        // fib*34) getting 1.630 s on dev
-        // fib(34) getting 0.798 s on rel. Now getting 764 mS - no idea what changed
-        debug::set(0, true);
-        let start_time = hal::timer::current_time();
-        fib(34);
-        let end_time = hal::timer::current_time();
-        debug::set(0, false);
-
-        let duration = end_time.sub(start_time);
-        b"  Duration: ".print_console();
-        let duration_ms = (duration.as_u64()) / 1000; // convert to mS
-        duration_ms.print_console();
-        b" mS\r\n".print_console();
-
-        //let (val,changed) = bsp.button.read_ptt();
-        //if val && changed {
-        //    b"  Direct PTT button pressed\r\n".print_console();
-        //}
-
-        //let (val,changed) = bsp.button.read_ai();
-        //if val && changed {
-        //    b"  Direct AI button pressed\r\n".print_console();
-        //}
-
-        dev::led::set(Color::Blue);
-
-        fib(32);
-
-        let stack_usage = stack::usage() as u32;
-
-        if cfg!(not(feature = "std")) {
-            b"  Stack usage: ".print_console();
-            stack_usage.print_console();
-            b" bytes\r\n".print_console();
-        }
-
-        b"\r\n".print_console();
+        dispatch::process(receiver);
 
         #[cfg(feature = "exit")]
         {
@@ -126,6 +79,27 @@ fn my_main() -> ! {
             hal::semihost::exit(0);
         }
     }
+}
+
+pub fn fib_test() {
+    // fib*34) getting 1.630 s on dev
+    // fib(34) getting 0.798 s on rel. Now getting 764 mS - no idea what changed
+
+    dev::led::set(Color::Blue);
+
+    debug::set(0, true);
+    let start_time = hal::timer::current_time();
+    fib(34);
+    let end_time = hal::timer::current_time();
+    debug::set(0, false);
+
+    led::set(Color::Green);
+
+    let duration = end_time.sub(start_time);
+    b"  Duration fib(34): ".print_console();
+    let duration_ms = (duration.as_u64()) / 1000; // convert to mS
+    duration_ms.print_console();
+    b" mS\r\n".print_console();
 }
 
 pub fn fib(x: usize) -> u32 {
