1593183951723e2c8d01276e6017a4c70d92a3a6
Cullen Jennings <fluffy@iii.ca>
2025-01-01 11:37:03 -0700

    Write doc for debug module

--- crates/dev/src/debug.rs
+++ crates/dev/src/debug.rs
@@ -1,3 +1,25 @@
+//! # Debug Module
+//!
+//! This module provides functionality for initializing and controlling debug pins for the board.
+//!
+//!
+//! ## Functions
+//!
+//! - `set`: Sets the state of a specified debug pin.
+//!
+//! ## Example
+//!
+//! ```rust
+//!  use dev::BSP;
+//!  use dev::debug;
+//!  let mut bsp = BSP::new();
+//!  bsp.init();
+//!
+//! // Set the debug pin 0 to high
+//! dev::debug::set(0, true);
+//! ```
+//!
+
 extern crate hal;
 
 use hal::board;
@@ -17,10 +39,39 @@ impl Debug {
     }
 }
 
-pub fn set(ch: u8, v: bool) {
-    assert!(ch == 0);
+/// Sets the state of a specified debug pin.
+///
+/// This function sets the state of the debug pin identified by the given channel number.
+/// The pin can be set to either high or low based on the boolean value provided.
+///
+/// # Arguments
+///
+/// * `ch` - The channel number of the debug pin. Currently only 0 is supported.
+/// * `v` - The state to set the debug pin to (`true` for high, `false` for low).
+///
+/// # Panics
+///
+/// This function will panic if the channel number is not 0.
+///
+/// # Example
+///
+/// ```rust
+/// use dev::BSP;
+/// use dev::debug;
+/// let mut bsp = BSP::new();
+/// bsp.init();
+///
+/// // Set the debug pin 0 to high
+/// debug::set(0, true);
+///
+/// // Set the debug pin 0 to low
+/// debug::set(0, false);
+/// ```
+///
+pub fn set(channel: u8, state: bool) {
+    assert!(channel == 0);
 
-    if v {
+    if state {
         board::info::DEBUG1_PIN.high();
     } else {
         board::info::DEBUG1_PIN.low();
