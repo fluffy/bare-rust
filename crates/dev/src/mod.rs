fc36cc8063644559a27f5a4dc512763f2962e521
Cullen Jennings <fluffy@iii.ca>
2025-01-01 10:59:35 -0700

    start dev module doc

--- crates/dev/src/mod.rs
+++ crates/dev/src/mod.rs
@@ -1,5 +1,41 @@
 #![no_std]
 
+//! # Board Support Package (BSP)
+//!
+//! This module provides the Board Support Package (BSP) for the project,
+//! which includes initialization and validation of various hardware
+//! components such as buttons, console, debug interface, and LEDs.
+//!
+//! ## Modules
+//!
+//! - `buttons`: Manages the hardware input button
+//! - `console`: Manages the console interface to print debug messages.
+//! - `debug`: Manages the debug interface that outputs signals to the debug pins
+//! - `led`: Manages the output of the LED on the board
+//!
+//! ## Structs
+//!
+//! - `BSP`: Represents the Board Support Package,
+//!    providing methods to initialize and validate the hardware components.
+//!
+//! ## Usage
+//!
+//! To use this module, create an instance of `BSP` and
+//! call the `init` method to initialize the hardware components.
+//!
+//! ## Example
+//!
+//! ```rust
+//!  use dev::BSP;
+//!  use dev::led;
+//!
+//!  let mut bsp = BSP::new();
+//!  bsp.init();
+//!
+//!  led::set(led::Color::Green);
+//! ```
+//!
+
 extern crate hal;
 
 pub mod buttons;
