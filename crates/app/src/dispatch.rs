6d898535205de07599fc52b9d5c2f687fb87bbaf
Cullen Jennings <fluffy@iii.ca>
2025-01-01 17:59:55 -0700

    add dispatch doc

--- crates/app/src/dispatch.rs
+++ crates/app/src/dispatch.rs
@@ -7,6 +7,12 @@ use crate::channel::v_mpsc;
 use crate::msg;
 use dev::console::Print;
 
+/// Processes all the incoming messages from tasks and dispatches them to the 
+/// appropriate task for handling.
+///
+/// Consumes all the messages from the receiver and 
+/// processes them each time it is called.
+///
 pub fn process(receiver: v_mpsc::Receiver<msg::Msg>) {
     loop {
         let msg = receiver.recv();
