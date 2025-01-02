3717f6d5d76e02d1258897ae66ab689730b658a1
Cullen Jennings <fluffy@iii.ca>
2025-01-01 17:27:58 -0700

    doc for button_tasks module

--- crates/app/src/tasks/buttons_task.rs
+++ crates/app/src/tasks/buttons_task.rs
@@ -1,20 +1,27 @@
+/// The `buttons_task` module contains the implementation of the button task.
+/// This task is responsible for reading the state of a button and sending a message
+/// if the button state changes.
+
 use super::Task;
 use crate::metrics::Metrics;
 use crate::msg::Msg;
 use crate::tasks::TaskInfo;
 
+/// Structure representing the button task.
 pub struct ButtonTask {
-    //pub prev_state: bool,
 }
 
+/// Information about the button task.
 const BUTTON_TASK_INFO: TaskInfo = TaskInfo {
     name: "Button",
     run_every_us: 100_000,
-    time_budget_us: 5000,
-    mem_budget_bytes: 500,
+    time_budget_us: 1_000,
+    mem_budget_bytes: 100,
 };
 
 impl Task for ButtonTask {
+    /// Method to execute the button task.
+    /// Reads the state of the button and sends a message if the state has changed.
     fn run(
         &self,
         _incoming_msg: &Msg,
@@ -29,6 +36,7 @@ impl Task for ButtonTask {
         }
     }
 
+    /// Returns the information about the button task.
     fn info(&self) -> &'static TaskInfo {
         &BUTTON_TASK_INFO
     }
