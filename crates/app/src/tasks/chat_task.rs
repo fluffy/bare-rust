0e60e29e2d6bbf6a07f697478da3488e908d24db
Cullen Jennings <fluffy@iii.ca>
2025-01-04 21:34:55 -0700

    start chat task

---
+++ crates/app/src/tasks/chat_task.rs
@@ -0,0 +1,36 @@
+
+
+use super::Task;
+use crate::metrics::Metrics;
+use crate::msg::Msg;
+use crate::tasks::TaskInfo;
+
+/// Structure representing the chat task.
+pub struct ChatTask {}
+
+/// Information about the chat task.
+const CHAT_TASK_INFO: TaskInfo = TaskInfo {
+    name: "Chat",
+    run_every_us: 100_000,
+    time_budget_us: 10_000,
+    mem_budget_bytes: 300,
+};
+
+impl Task for ChatTask {
+    /// Method to execute the chat task.
+    /// Reads the state of the chat and sends a message if the state has changed.
+    fn run(
+        &self,
+        _incoming_msg: &Msg,
+        _sender: &mut crate::mpsc::Sender<Msg>,
+        _bsp: &mut bsp::BSP,
+        _metrics: &mut Metrics,
+    ) {
+
+    }
+
+    /// Returns the information about the chat task.
+    fn info(&self) -> &'static TaskInfo {
+        &CHAT_TASK_INFO
+    }
+}
