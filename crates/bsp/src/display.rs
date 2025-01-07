d1d0e50fc31721a0d385a333562bc71e2bb56683
Cullen Jennings <fluffy@iii.ca>
2025-01-06 18:05:53 -0700

    update display to just be async bitmap transfer

--- crates/bsp/src/display.rs
+++ crates/bsp/src/display.rs
@@ -1,16 +1,11 @@
 //! # Display Module
 //!
 //! This module provides functionality for controlling the display on the board.
-//! It includes methods for initializing the display, drawing pixels, bitmaps, and rectangles
-//! It also has function that needs to be periodically called for refreshing the display to the LCD.
 //!
 //! ## Functions
 //!
 //! - `size`: Returns the width and height of the display.
-//! - `draw_pixel`: Draws a pixel at a specified position.
 //! - `draw_bitmap`: Draws a bitmap at a specified position.
-//! - `draw_rect`: Draws a rectangle at a specified position with the specified width, height, and color.
-//! - `refresh`: Refreshes the display.
 //!
 //! ## Example
 //!
@@ -19,21 +14,20 @@
 //!
 //! let display = Display::new();
 //! display.init();
-//!
-//! display.draw_pixel(10, 10, 0xFFFF);
-//! display.draw_bitmap(0, 0, &[0xFFFF; 128 * 64], 128, 64, 128);
-//! display.draw_rect(20, 20, 50, 50, 0xFFFF);
-//!
-//! display.refresh();
+//! 
+//! display.draw_bitmap( &[0xFFFF; 240 * 320/10], 0,0, 240, 320/10, 240 );
+//! 
+//! while !display.ready() {}
 //! ```
+//! 
 
 extern crate hal;
 
 #[cfg(feature = "std")]
 extern crate std;
 
-const WIDTH: usize = 32;
-const HEIGHT: usize = 32;
+const WIDTH: usize = 240;
+const HEIGHT: usize = 320;
 
 /// Struct keeping track of the display state
 pub struct Display {
@@ -54,34 +48,25 @@ impl Display {
         (WIDTH as u32, HEIGHT as u32)
     }
     
-
+    /// Returns whether the display is ready to accept the next draw_bitmap command.
+    pub fn ready(&self) -> bool {
+        true
+    }
+    
     /// Draws a bitmap at a specified position.
+    /// 
+    /// This is an async call that starts a DMA transfer to the display controller and 
+    /// ready will return false until the transfer is complete.
     pub fn draw_bitmap(
         &self,
+        bitmap: &[u16],
         x: u32,
         y: u32,
-        bitmap: &[u16],
         width: u32,
         height: u32,
         stride: u32,
     ) {
         // Draw a bitmap at position (x, y)
-        let _unused = (x, y, bitmap, width, height, stride);
-    }
-
-    /// Draws a rectangle at a specified position with the specified width, height, and color.
-    pub fn draw_rect(&self, x: u32, y: u32, width: u32, height: u32, color: u16) {
-        // Draw a rectangle at position (x, y) with the specified width and height
-        let _unused = (x, y, width, height, color);
-    }
-
-    /// Partially refreshes the display.
-    ///
-    /// This function needs to be called periodically to refresh the display
-    /// and only refreshes some of  the rows that are marked as dirty. It needs to be called
-    /// multiple times to refresh the entire display.
-    ///
-    pub fn refresh(&self) {
-        // Refresh the display
+        let _unused = ( bitmap, x,y, width, height, stride);
     }
 }
