1044ba52cdeca90ab4be8e1f15dfc1b7e0a630b1
Cullen Jennings <fluffy@iii.ca>
2025-01-11 20:37:30 -0700

    add stm32f072 registers

--- crates/hal/src/cpu.rs
+++ crates/hal/src/cpu.rs
@@ -46,6 +46,7 @@
 //! behavior if used incorrectly.
 //!
 
+
 #[cfg(feature = "stm32f405")]
 use super::svd_stm32f405 as svd;
 
@@ -63,6 +64,7 @@ use std::collections::HashMap;
 #[cfg(feature = "std")]
 use std::sync::Mutex;
 
+#[cfg(feature = "stm32f405")]
 #[repr(C)]
 pub struct FlashReg {
     pub acr: u32,
@@ -73,8 +75,12 @@ pub struct FlashReg {
     pub optcr: u32,
 }
 
+#[cfg(feature = "stm32f405")]
 pub const FLASH: *mut FlashReg = 0x4002_3C00 as *mut FlashReg;
 
+
+
+#[cfg(feature = "stm32f405")]
 #[repr(C)]
 pub struct RccReg {
     pub cr: u32,
@@ -121,8 +127,68 @@ pub struct RccReg {
     pub pli2scfgr: u32,
 }
 
+#[cfg(feature = "stm32f072")]
+#[repr(C)]
+pub struct RccReg {
+    pub cr: u32,
+    //pub pllcfgr: u32,
+    pub cfgr: u32,
+    pub cir: u32,
+
+    pub ahb2rstr: u32,
+    pub ahb1rstr: u32,
+    //pub ahb3rstr: u32,
+    //reserved1: u32,
+
+    pub ahbenr: u32,
+    
+    //pub apb1_enr: u32,
+    //pub apb12enr: u32,
+    //reserved2: u32,
+    //reserved3: u32,
+
+    //pub ahb2enr: u32,
+    //pub ahb1enr: u32,
+    //pub ahb3enr: u32,
+    //reserved4: u32,
+
+    pub apb2enr: u32,
+    pub apb1enr: u32,
+    //reserved5: u32,
+    //reserved6: u32,
+
+    //pub ahb1lpenr: u32,
+    //pub ahb2lpenr: u32,
+    //pub ahb3lpenr: u32,
+    //reserved7: u32,
+
+    //pub apb1lpenr: u32,
+    //pub apb2lpenr: u32,
+    //reserved8: u32,
+    //reserved9: u32,
+
+    pub bdcr: u32,
+    pub csr: u32,
+    //reserved10: u32,
+    //reserved11: u32,
+
+    pub ahbrstr: u32, 
+    pub cfgr2: u32, 
+    pub cfgr3: u32, 
+    pub cr2: u32, 
+    //pub sscgr: u32,
+    //pub pli2scfgr: u32,
+}
+
+#[cfg(feature = "stm32f405")]
 pub const RCC: *mut RccReg = 0x4002_3800 as *mut RccReg;
 
+
+#[cfg(feature = "stm32f072")]
+pub const RCC: *mut RccReg = 0x4002_1000 as *mut RccReg;
+
+
+#[cfg(feature = "stm32f405")]
 #[repr(C)]
 pub struct NVICReg {
     pub iser: [u32; 8], // Interrupt Set-Enable Registers
@@ -133,8 +199,11 @@ pub struct NVICReg {
     pub ipr: [u32; 60], // Interrupt Priority Registers
     pub stir: u32,      // Software Trigger Interrupt Register
 }
+
+#[cfg(feature = "stm32f405")]
 pub const NVIC: *mut NVICReg = 0xE000_E100 as *mut NVICReg;
 
+#[cfg(feature = "stm32f405")]
 #[repr(C)]
 pub struct GpioReg {
     pub moder: u32,
@@ -150,13 +219,45 @@ pub struct GpioReg {
     pub brr: u32,
 }
 
+#[cfg(feature = "stm32f072")]
+#[repr(C)]
+pub struct GpioReg {
+    pub moder: u32,
+    pub otyper: u32,
+    pub ospeedr: u32,
+    pub pupdr: u32,
+    pub idr: u32,
+    pub odr: u32,
+    pub bsrr: u32,
+    pub lckr: u32,
+    pub afrl: u32,
+    pub afrh: u32,
+    pub brr: u32,
+}
+
+#[cfg(feature = "stm32f405")]
 #[allow(unused)]
 pub const GPIO_A: *mut GpioReg = 0x4002_0000 as *mut GpioReg;
+
+#[cfg(feature = "stm32f072")]
+#[allow(unused)]
+pub const GPIO_A: *mut GpioReg = 0x4800_0000 as *mut GpioReg;
+
+#[cfg(feature = "stm32f405")]
 #[allow(unused)]
 pub const GPIO_B: *mut GpioReg = 0x4002_0400 as *mut GpioReg;
+
+#[cfg(feature = "stm32f072")]
+#[allow(unused)]
+pub const GPIO_B: *mut GpioReg = 0x4800_0400 as *mut GpioReg;
+
+
+#[cfg(feature = "stm32f405")]
 #[allow(unused)]
 pub const GPIO_C: *mut GpioReg = 0x4002_0800 as *mut GpioReg;
 
+
+#[cfg(feature = "stm32f405")]
 #[repr(C)]
 pub struct UsartReg {
     pub sr: u32,
@@ -168,8 +269,33 @@ pub struct UsartReg {
     pub gtpr: u32,
 }
 
+
+#[cfg(feature = "stm32f072")]
+#[repr(C)]
+pub struct UsartReg {
+    pub cr1: u32,
+    pub cr2: u32,
+    pub cr3: u32,
+    pub brr: u32,
+    pub gtpr: u32,
+ 
+    pub rtor: u32,
+    pub rqr: u32,
+    pub isr: u32,
+    pub icr: u32,
+    pub rdr: u32,
+    pub tdr: u32,
+}
+ 
+#[cfg(feature = "stm32f405")]
 pub const USART1: *mut UsartReg = 0x4001_1000 as *mut UsartReg;
 
+
+#[cfg(feature = "stm32f072")]
+pub const USART1: *mut UsartReg =0x4001_3800 as *mut UsartReg;
+
+
+#[cfg(feature = "stm32f405")]
 #[repr(C)]
 pub struct TimAdvReg {
     pub cr1: u32,
@@ -193,8 +319,11 @@ pub struct TimAdvReg {
     pub dcr: u32,
     pub dmar: u32,
 }
+
+#[cfg(feature = "stm32f405")]
 pub const TIM1: *mut TimAdvReg = 0x4001_0000 as *mut TimAdvReg;
 
+#[cfg(feature = "stm32f405")]
 #[repr(C)]
 pub struct TimGenReg {
     pub cr1: u32,
@@ -218,6 +347,8 @@ pub struct TimGenReg {
     pub dcr: u32,
     pub dmar: u32,
 }
+
+#[cfg(feature = "stm32f405")]
 pub const TIM2: *mut TimGenReg = 0x4000_0000 as *mut TimGenReg;
 
 #[inline(always)]
