acae009dd773825803564b739b16218957cd7671
Cullen Jennings <fluffy@iii.ca>
2025-01-25 11:31:42 -0800

    edits to align with new gen

--- crates/hal/src/svd_stm32f405.rs
+++ crates/hal/src/svd_stm32f405.rs
@@ -728,6 +728,190 @@ pub mod RCC {
     }
 }
 
+pub mod GPIO {
+    pub mod moder {
+        pub const MODER15: u8 = 30;
+        pub const MODER14: u8 = 28;
+        pub const MODER13: u8 = 26;
+        pub const MODER12: u8 = 24;
+        pub const MODER11: u8 = 22;
+        pub const MODER10: u8 = 20;
+        pub const MODER9: u8 = 18;
+        pub const MODER8: u8 = 16;
+        pub const MODER7: u8 = 14;
+        pub const MODER6: u8 = 12;
+        pub const MODER5: u8 = 10;
+        pub const MODER4: u8 = 8;
+        pub const MODER3: u8 = 6;
+        pub const MODER2: u8 = 4;
+        pub const MODER1: u8 = 2;
+        pub const MODER0: u8 = 0;
+    }
+    pub mod otyper {
+        pub const OT15: u8 = 15;
+        pub const OT14: u8 = 14;
+        pub const OT13: u8 = 13;
+        pub const OT12: u8 = 12;
+        pub const OT11: u8 = 11;
+        pub const OT10: u8 = 10;
+        pub const OT9: u8 = 9;
+        pub const OT8: u8 = 8;
+        pub const OT7: u8 = 7;
+        pub const OT6: u8 = 6;
+        pub const OT5: u8 = 5;
+        pub const OT4: u8 = 4;
+        pub const OT3: u8 = 3;
+        pub const OT2: u8 = 2;
+        pub const OT1: u8 = 1;
+        pub const OT0: u8 = 0;
+    }
+    pub mod ospeedr {
+        pub const OSPEEDR15: u8 = 30;
+        pub const OSPEEDR14: u8 = 28;
+        pub const OSPEEDR13: u8 = 26;
+        pub const OSPEEDR12: u8 = 24;
+        pub const OSPEEDR11: u8 = 22;
+        pub const OSPEEDR10: u8 = 20;
+        pub const OSPEEDR9: u8 = 18;
+        pub const OSPEEDR8: u8 = 16;
+        pub const OSPEEDR7: u8 = 14;
+        pub const OSPEEDR6: u8 = 12;
+        pub const OSPEEDR5: u8 = 10;
+        pub const OSPEEDR4: u8 = 8;
+        pub const OSPEEDR3: u8 = 6;
+        pub const OSPEEDR2: u8 = 4;
+        pub const OSPEEDR1: u8 = 2;
+        pub const OSPEEDR0: u8 = 0;
+    }
+    pub mod pupdr {
+        pub const PUPDR15: u8 = 30;
+        pub const PUPDR14: u8 = 28;
+        pub const PUPDR13: u8 = 26;
+        pub const PUPDR12: u8 = 24;
+        pub const PUPDR11: u8 = 22;
+        pub const PUPDR10: u8 = 20;
+        pub const PUPDR9: u8 = 18;
+        pub const PUPDR8: u8 = 16;
+        pub const PUPDR7: u8 = 14;
+        pub const PUPDR6: u8 = 12;
+        pub const PUPDR5: u8 = 10;
+        pub const PUPDR4: u8 = 8;
+        pub const PUPDR3: u8 = 6;
+        pub const PUPDR2: u8 = 4;
+        pub const PUPDR1: u8 = 2;
+        pub const PUPDR0: u8 = 0;
+    }
+    pub mod idr {
+        pub const IDR15: u8 = 15;
+        pub const IDR14: u8 = 14;
+        pub const IDR13: u8 = 13;
+        pub const IDR12: u8 = 12;
+        pub const IDR11: u8 = 11;
+        pub const IDR10: u8 = 10;
+        pub const IDR9: u8 = 9;
+        pub const IDR8: u8 = 8;
+        pub const IDR7: u8 = 7;
+        pub const IDR6: u8 = 6;
+        pub const IDR5: u8 = 5;
+        pub const IDR4: u8 = 4;
+        pub const IDR3: u8 = 3;
+        pub const IDR2: u8 = 2;
+        pub const IDR1: u8 = 1;
+        pub const IDR0: u8 = 0;
+    }
+    pub mod odr {
+        pub const ODR15: u8 = 15;
+        pub const ODR14: u8 = 14;
+        pub const ODR13: u8 = 13;
+        pub const ODR12: u8 = 12;
+        pub const ODR11: u8 = 11;
+        pub const ODR10: u8 = 10;
+        pub const ODR9: u8 = 9;
+        pub const ODR8: u8 = 8;
+        pub const ODR7: u8 = 7;
+        pub const ODR6: u8 = 6;
+        pub const ODR5: u8 = 5;
+        pub const ODR4: u8 = 4;
+        pub const ODR3: u8 = 3;
+        pub const ODR2: u8 = 2;
+        pub const ODR1: u8 = 1;
+        pub const ODR0: u8 = 0;
+    }
+    pub mod bsrr {
+        pub const BR15: u8 = 31;
+        pub const BR14: u8 = 30;
+        pub const BR13: u8 = 29;
+        pub const BR12: u8 = 28;
+        pub const BR11: u8 = 27;
+        pub const BR10: u8 = 26;
+        pub const BR9: u8 = 25;
+        pub const BR8: u8 = 24;
+        pub const BR7: u8 = 23;
+        pub const BR6: u8 = 22;
+        pub const BR5: u8 = 21;
+        pub const BR4: u8 = 20;
+        pub const BR3: u8 = 19;
+        pub const BR2: u8 = 18;
+        pub const BR1: u8 = 17;
+        pub const BR0: u8 = 16;
+        pub const BS15: u8 = 15;
+        pub const BS14: u8 = 14;
+        pub const BS13: u8 = 13;
+        pub const BS12: u8 = 12;
+        pub const BS11: u8 = 11;
+        pub const BS10: u8 = 10;
+        pub const BS9: u8 = 9;
+        pub const BS8: u8 = 8;
+        pub const BS7: u8 = 7;
+        pub const BS6: u8 = 6;
+        pub const BS5: u8 = 5;
+        pub const BS4: u8 = 4;
+        pub const BS3: u8 = 3;
+        pub const BS2: u8 = 2;
+        pub const BS1: u8 = 1;
+        pub const BS0: u8 = 0;
+    }
+    pub mod lckr {
+        pub const LCKK: u8 = 16;
+        pub const LCK15: u8 = 15;
+        pub const LCK14: u8 = 14;
+        pub const LCK13: u8 = 13;
+        pub const LCK12: u8 = 12;
+        pub const LCK11: u8 = 11;
+        pub const LCK10: u8 = 10;
+        pub const LCK9: u8 = 9;
+        pub const LCK8: u8 = 8;
+        pub const LCK7: u8 = 7;
+        pub const LCK6: u8 = 6;
+        pub const LCK5: u8 = 5;
+        pub const LCK4: u8 = 4;
+        pub const LCK3: u8 = 3;
+        pub const LCK2: u8 = 2;
+        pub const LCK1: u8 = 1;
+        pub const LCK0: u8 = 0;
+    }
+    pub mod afrl {
+        pub const AFRL7: u8 = 28;
+        pub const AFRL6: u8 = 24;
+        pub const AFRL5: u8 = 20;
+        pub const AFRL4: u8 = 16;
+        pub const AFRL3: u8 = 12;
+        pub const AFRL2: u8 = 8;
+        pub const AFRL1: u8 = 4;
+        pub const AFRL0: u8 = 0;
+    }
+    pub mod afrh {
+        pub const AFRH15: u8 = 28;
+        pub const AFRH14: u8 = 24;
+        pub const AFRH13: u8 = 20;
+        pub const AFRH12: u8 = 16;
+        pub const AFRH11: u8 = 12;
+        pub const AFRH10: u8 = 8;
+        pub const AFRH9: u8 = 4;
+        pub const AFRH8: u8 = 0;
+    }
+}
+
 pub mod USART {
     pub mod sr {
         pub const CTS: u8 = 9;
@@ -1169,9 +1353,6 @@ pub mod FLASH {
     }
 }
 
-
-
-
 #[repr(C)]
 pub struct DmaReg {
     pub lisr: u32,
@@ -1228,7 +1409,6 @@ pub struct DmaReg {
     pub s7fcr: u32,
 }
 
-
 #[repr(C)]
 pub struct RccReg {
     pub cr: u32,
@@ -1275,7 +1455,6 @@ pub struct RccReg {
     pub pli2scfgr: u32,
 }
 
-
 #[repr(C)]
 pub struct GpioReg {
     pub moder: u32,
@@ -1291,8 +1470,6 @@ pub struct GpioReg {
     pub brr: u32,
 }
 
-
-
 #[repr(C)]
 pub struct UsartReg {
     pub sr: u32,
@@ -1304,9 +1481,8 @@ pub struct UsartReg {
     pub gtpr: u32,
 }
 
-
 #[repr(C)]
-pub struct TIM_ADVReg {
+pub struct TimAdvReg {
     pub cr1: u32,
     pub cr2: u32,
     pub smcr: u32,
@@ -1328,7 +1504,6 @@ pub struct TIM_ADVReg {
     pub dcr: u32,
     pub dmar: u32,
 }
-
 
 #[repr(C)]
 pub struct TimGenReg {
@@ -1354,7 +1529,6 @@ pub struct TimGenReg {
     pub dmar: u32,
 }
 
-
 #[repr(C)]
 pub struct FlashReg {
     pub acr: u32,
@@ -1364,8 +1538,6 @@ pub struct FlashReg {
     pub cr: u32,
     pub optcr: u32,
 }
-
-
 
 pub const DMA2: *mut DmaReg = 0x40026400 as *mut DmaReg;
 
@@ -1379,15 +1551,8 @@ pub const GPIO_A: *mut GpioReg = 0x40020000 as *mut GpioReg;
 
 pub const USART1: *mut UsartReg = 0x40011000 as *mut UsartReg;
 
-pub const TIM1: *mut TIM_ADVReg = 0x40010000 as *mut TIM_ADVReg;
+pub const TIM1: *mut TimAdvReg = 0x40010000 as *mut TimAdvReg;
 
 pub const TIM2: *mut TimGenReg = 0x40000000 as *mut TimGenReg;
 
 pub const FLASH: *mut FlashReg = 0x40023C00 as *mut FlashReg;
-
-
-
-//pub const DMA1: *mut DmaReg = 0x4002_6000 as *mut DmaReg;
-
-
-
