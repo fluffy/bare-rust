2be0c0f76fbda37434fefb98c4b904924ed4ae3e
Cullen Jennings <fluffy@iii.ca>
2025-02-09 18:18:10 -0700

    Start display chip setup

--- crates/bsp/src/display.rs
+++ crates/bsp/src/display.rs
@@ -60,6 +60,8 @@ impl Display {
             board::info::DISP_SCL,
             board::info::DISP_SDA,
         );
+
+        ili9341::setup();
     }
 
     /// Returns the width and height of the display.
@@ -87,5 +89,148 @@ impl Display {
     ) {
         // Draw a bitmap at position (x, y)
         let _unused = (bitmap, x, y, width, height, stride);
+    }
+}
+
+mod ili9341 {
+    use crate::board;
+    use hal::uart;
+
+    #[allow(dead_code)]
+    pub enum Command {
+        NoOp = 0x00,       // No operation
+        SwReset = 0x01,    // Software reset
+        SleepOut = 0x11,   // Sleep out
+        NormalMode = 0x13, // Normal mode
+
+        GammaSet = 0x26,      // Gamma set
+        DisplayOff = 0x28,    // Display off
+        DisplayOn = 0x29,     // Display on
+        ColumnAddrSet = 0x2A, // Column address set
+        PageAddrSet = 0x2B,   // Page address set
+        MemoryWrite = 0x2C,   // Memory write
+
+        PixelFormatSet = 0x3A,   // Pixel format set
+        MemoryAccessCtrl = 0x36, // Memory access control
+
+        SetTearingEffectLineOff = 0x34, // Tearing effect line off
+        SetTearingEffectLineOn = 0x35,  // Tearing effect line on
+
+        SetTearScanline = 0x44, // Set tear scanline
+
+        FraameRateCtrl = 0xB1, // Frame rate control (In normal mode/Full colors)
+        DisplayFunctionCtrl = 0xB6, // Display function control
+
+        PowerCtrl1 = 0xC0, // Power control 1
+        PowerCtrl2 = 0xC1, // Power control 2
+        PowerCtrlB = 0xCF, // Power control B (CFh)
+        PowerCtrlA = 0xCB, // Power control A (CBh)
+        VcomCtrl1 = 0xC5,  // VCOM control 1 (C5h)
+        VcomCtrl2 = 0xC7,  // VCOM control 2 (C7h)
+
+        PositiveGammaCorrect = 0xE0, // Positive gamma correction
+        NegativeGammaCorrect = 0xE1, // Negative gamma correction
+
+        DriverTimingCtrlA = 0xE8, // Driver timing control A (E8h)
+        DriverTimingCtrlB = 0xEA, // Driver timing control B (EAh)
+        PowerOnSeqCtrl = 0xED,    // Power on sequence control (EDh)
+
+        Enable3GammaCtrl = 0xF2, // Enable 3 gamma control (F2h)
+        PumpRatioCtrl = 0xF7,    // Pump ratio control (F7h)
+    }
+
+    pub fn command(cmd: Command, paramters: &[u8]) {
+        // Send a command to the display
+        let c: [u8; 1] = [cmd as u8];
+
+        board::info::DISP_DC.low(); // command
+        hal::spi::write1(&c);
+
+        board::info::DISP_DC.high(); // data
+        hal::spi::write1(paramters);
+    }
+
+    pub fn setup() {
+        //LCD_2IN4_Write_Command(0x01); //Software reset
+        command(Command::SwReset, &[]);
+
+        for c in b"Starting display setup\r\n" {
+            uart::write1(*c);
+        }
+        // TODO - add 50 ms delay, remove print
+
+        command(Command::PowerCtrlB, &[0x00, 0xC1, 0x30]);
+
+        command(Command::PowerOnSeqCtrl, &[0x64, 0x03, 0x12, 0x81]);
+
+        command(Command::DriverTimingCtrlA, &[0x85, 0x00, 0x79]); // 78 or 79 ???
+
+        command(Command::PowerCtrlA, &[0x39, 0x2C, 0x00, 0x34, 0x02]);
+
+        command(Command::PumpRatioCtrl, &[0x20]);
+
+        command(Command::DriverTimingCtrlB, &[0x00, 0x00]);
+
+        command(Command::PowerCtrl1, &[0x1D]); // or 0x23 ???
+
+        command(Command::PowerCtrl2, &[0x12]); // or 0x10 ???
+
+        command(Command::VcomCtrl1, &[0x33, 0x3F]); // or 3e,28
+
+        command(Command::VcomCtrl2, &[0x92]); // or 86 ???
+
+        command(Command::PixelFormatSet, &[0x55]);
+
+        command(Command::MemoryAccessCtrl, &[0x08]); // or 0x48 or 0x88
+
+        command(Command::FraameRateCtrl, &[0x00, 0x12]); // or 18
+
+        // TODO takes 4 parameters 
+        command(Command::DisplayFunctionCtrl, &[0x0A, 0xA2]); // perhaps  0x08, 0x82, 0x27 ???
+        //command(Command::DisplayFunctionCtrl, &[0x08, 0x82, 0x27]); 
+
+        command(Command::SetTearScanline, &[0x02]);
+
+        command(Command::Enable3GammaCtrl, &[0x00]);
+
+        command(Command::GammaSet, &[0x01]);
+
+        command(
+            Command::PositiveGammaCorrect,
+            &[
+                0x0F, 0x22, 0x1C, 0x1B, 0x08, 0x0F, 0x48, 0xB8, 0x34, 0x05, 0x0C, 0x09, 0x0F, 0x07,
+                0x00,
+                // alternative values
+                // 0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08, 0x4E, 0xF1, 0x37, 0x07, 0x10, 0x03, 0x0E, 0x09,
+                // 0x00
+            ],
+        );
+
+        command(
+            Command::NegativeGammaCorrect,
+            &[
+                0x00, 0x23, 0x24, 0x07, 0x10, 0x07, 0x38, 0x47, 0x4B, 0x0A, 0x13, 0x06, 0x30, 0x38,
+                0x0F,
+                // alternative values
+                // 0x00, 0x0E, 0x14, 0x03, 0x11, 0x07, 0x31, 0xC1, 0x48, 0x08, 0x0F, 0x0C, 0x31, 0x36,
+                // 0x0F
+            ],
+        );
+
+        command(Command::NormalMode, &[]);
+
+        command(Command::SleepOut, &[]);
+        // TODO wait 120 ms
+
+        command(Command::DisplayOn, &[]);
+
+        if true {
+            command(Command::ColumnAddrSet, &[0x00, 10, 0x00, 100]); // EF=239
+            command(Command::PageAddrSet, &[0x00, 10, 0x00, 100]); // 13F=319
+
+            let data = [128u8; 90 * 90 * 2];
+            command(Command::MemoryWrite, &data);
+            command(Command::NoOp, &[]);
+        }
     }
 }
