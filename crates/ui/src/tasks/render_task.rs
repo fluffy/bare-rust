b690f8ab14b017e983a6c1fb83ce4419509b1f7a
Cullen Jennings <fluffy@iii.ca>
2025-02-12 21:44:07 -0700

    start font render code

--- crates/ui/src/tasks/render_task.rs
+++ crates/ui/src/tasks/render_task.rs
@@ -4,6 +4,8 @@ use crate::msg::Msg;
 use crate::tasks::TaskInfo;
 use bsp::board;
 
+use crate::font;
+
 /// Structure representing the render task.
 pub struct RenderTask {}
 
@@ -146,10 +148,101 @@ impl Task for RenderTask {
 
         for y in 0..DISPLAY_BAND_HEIGHT {
             for x in 0..DISPLAY_WIDTH {
-                data.bitmap[y * DISPLAY_WIDTH + x] = color; // red
+                data.bitmap[y * DISPLAY_WIDTH + x] = 0xFFFF; // red
             }
         }
 
+        let mut x : i32 = 5;
+        let y : i32 = font::METRICS.ascent as i32 + 1;
+
+        let text = "Hello, AV!";
+        //let text = "Hello";
+
+        for c in text.chars() {
+            let index = font::GLYPH_INDEX[c as usize] as usize;
+           
+           if index == 255 {
+                continue;
+            }
+           
+            let glyph = &font::GLYPH_METRICS[index];
+
+            let rle_start = glyph.rle_start as usize;
+            let rle_end = font::RLE_DATA.len();
+
+            let width = glyph.width as i32;
+
+            let mut rle_index = rle_start;
+            let mut row: i32 = 0;
+            let mut col : i32 = 0;
+
+            let max_row = font::METRICS.ascent as i32 - font::METRICS.descent as i32;
+            let max_col = glyph.width as i32 + 1;
+
+            assert!( font::METRICS.min_width <= font::METRICS.max_width);
+            assert!( font::METRICS.rle_bytes > 0 );
+            assert!( font::METRICS.line_gap <= 2 );
+
+            assert!( glyph.xmin <= 5 );
+            assert!( glyph.xmin >= 0 );
+
+            let ymax = glyph.ymin + glyph.height as i8;
+
+            for _  in ymax ..  font::METRICS.ascent as i8 {
+                row += 1;
+                assert!( row <= max_row);
+            }
+
+            while rle_index < rle_end {
+                let (count,val) = font::RLE_DATA[rle_index];
+                if count == 0 && val == 0 {
+                    break;
+                }
+                if col == 0 {
+                    for _ in 0 .. glyph.xmin {
+                      
+                        col += 1;
+                        assert!( col <= max_col);
+                    }
+                }
+                if count == -1 {
+                    for _ in col ..width {
+                      
+                        col += 1;
+                        assert!( col <= max_col);
+                    }
+                   
+                    row += 1;
+                    assert!( row <= max_row);
+                    col = 0;
+                }
+                if  count > 0 {
+                    for _ in 0..count {
+                        if val < 255 {
+
+                            let red: u16 = (val as u16) >> (8 - 5);
+                            let green: u16 =  (val as u16)  >> (8 - 6);
+                            let blue: u16 =  (val as u16)  >> (8 - 5);
+                            let color: u16 = (red << 11) | (green << 5) | blue;
+                            
+                            data.bitmap[ ((y- (glyph.height as i32 ) +row ) * DISPLAY_WIDTH as i32 + x+col ) as usize] = color; 
+                            
+                            col += 1;
+                            assert!( col <= max_col);
+                        }
+                        else {
+                            col += 1;
+                            assert!( col <= max_col);
+                        }
+                    }
+                }
+
+                rle_index += 1;
+            }
+           
+           x += 1 + glyph.width as i32;
+        }
+        
         bsp.display.draw_bitmap(
             &data.bitmap,
             0,                                       // x
